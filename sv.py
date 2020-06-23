96aebb0e16861c357d336da6aee662d12a6e7b6c
q690723481 <690723481@qq.com>
2020-06-23 21:32:15 +0800

    Revert "Revert "Github""
    This reverts commit 9f51648425e6d9a330e2229c0b51311c3778def4.

--- sv.py
+++ sv.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
-import numpy as np
+#by wangxiaohan U201514480
+import numpy as np     
 import pandas as pd
 from sklearn.linear_model import SGDClassifier
 from matplotlib import pyplot as plt
